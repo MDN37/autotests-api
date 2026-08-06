9d2de7e4c054546fc4b89a1273c214acffa76bd8
MDN37 <valax2010@yandex.ru>
2026-08-06 16:27:12 +0300

    Маркировки Pytest

---
+++ tests/test_pytest_markers.py
@@ -0,0 +1,48 @@
+# import pytest
+#
+# @pytest.mark.smoke
+# def test_smoke_case():
+#     assert 1 + 1 == 2
+#
+# @pytest.mark.regression
+# def test_regression_case():
+#     assert 2 * 2 == 4
+#
+# @pytest.mark.fast
+# def test_fast():
+#     pass
+#
+# @pytest.mark.slow
+# def test_slow():
+#     pass
+#
+#
+# @pytest.mark.regression
+# class TestUserAuthentication:
+#
+#     @pytest.mark.smoke
+#     def test_login(self):
+#         pass
+#
+#     @pytest.mark.slow
+#     def test_password_reset(self):
+#         pass
+#
+#     def test_logout(self):
+#         pass
+#
+# @pytest.mark.api
+# class TestUserInterface:
+#
+#     @pytest.mark.smoke
+#     @pytest.mark.critical
+#     def test_login(self):
+#         pass
+#
+#     @pytest.mark.regression
+#     def test_forgot_password(self):
+#         pass
+#
+#     @pytest.mark.smoke
+#     def test_signup(self):
+#         pass
